218695ee5e98a9c0cb451558e233bed43f02fdf3
vahan-sahakyan <vahan.sahakyan@optimumpartners.co>
2024-05-28 16:26:16 +0400

    redirect on-poster-click, font-family Helvetica

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -6,16 +6,18 @@ import { IGenreMap, IMovie } from 'src/app/models/movie/movie.model';
   styleUrls: ['./movie-item.component.scss'],
   template: `
     <figure class="movie-item bg-white rounded-xl dark:bg-zinc-800">
-      <header class="movie-item__poster relative mb-4">
-        <div
-          class="movie-item__poster--sceleton w-full rounded-xl absolute bg-zinc-500 animate-pulse duration-75 opacity-25"
-        ></div>
-        <img
-          [src]="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
-          class="movie-item__poster--image w-full rounded-xl absolute shadow-zinc-400 dark:shadow-zinc-900 shadow-lg "
-        />
-        <div class="movie-item__poster--space rounded-xl opacity-0"></div>
-      </header>
+      <a [routerLink]="['/', movie.id]">
+        <header class="movie-item__poster relative mb-4">
+          <div
+            class="movie-item__poster--sceleton w-full rounded-xl absolute bg-zinc-500 animate-pulse duration-75 opacity-25"
+          ></div>
+          <img
+            [src]="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+            class="movie-item__poster--image w-full rounded-xl absolute shadow-zinc-400 dark:shadow-zinc-900 shadow-lg "
+          />
+          <div class="movie-item__poster--space rounded-xl opacity-0"></div>
+        </header>
+      </a>
 
       <footer>
         <p class="text-sm mb-0 dark:text-zinc-400 text-zinc-500">
